1c319890e8ee8c62067258dd741f778915fbc04c
BreakingDev24 <antonio.avolio12@gmail.com>
2023-03-23 21:10:01 +0100

    created getPlayerSelection, getComputerChoice and playRound function

---
+++ script.js
@@ -0,0 +1,44 @@
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerSelection();
+let playerPoint = 0;
+let computerPoint = 0;
+
+
+function getPlayerSelection() {
+    let selectionP = prompt("rock, paper or scissor?",).toLowerCase()
+    return selectionP
+}
+
+
+
+function getComputerChoice() {
+    let choice = ["rock", "paper", "scissor"];
+    let computerChoice = choice[Math.floor(Math.random() * choice.length)];
+    return computerChoice
+    
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    if (
+        (playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissor") ||
+        (playerSelection === "scissor" && computerSelection === "rock")
+        ) {
+            return `You Lose! ${computerSelection} beats ${playerSelection}`
+        } else if (playerSelection === computerSelection) {
+            return "it's a tie"
+        } else {
+            return `You Win! ${playerSelection} beats ${computerSelection}`
+        }
+        
+    }
+
+
+
+
+    
+
+
+
+
